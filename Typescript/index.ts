7f99dc90e5330414c4546cc3376ffc1ef2f0a0ce
Christopher C Fleming <63308959+ChristopherCFleming@users.noreply.github.com>
2023-04-24 15:39:10 -0400

    Create TypeScript Problems

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -12,6 +12,27 @@ console.log(x);
 let strArray: string[] = [];
 let strAr: Array<string> = [];
 
-let user: [number, string] = [9, 'nine'];
+let userInfo: [number, string] = [9, 'nine'];
+console.log(true);
 
-console.log(user);
+const small: number = 5;
+enum Size {small=5};
+
+console.log(Size);
+
+const person: {id: number, user: string} = {
+    id: 0,
+    user: 'Chris',
+}
+
+
+//Here, we say this function must take in a number and return a boolean
+function isEven (x: number): boolean {
+    if (x % 2 === 1) return false;
+    return true;
+}
+
+//Here, if a function calls another function or doesn't return anything itself, we must say it returns void. If it actually returns null, we have to say null.
+function doesntReturnAnything (x: number): void {
+    console.log('HelloWorld')
+}
